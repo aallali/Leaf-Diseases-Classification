e8f878e164aace5335109ea2096a4f8aa295deec
MoldyLunchBox <duu116@gmail.com>
2024-01-20 20:33:57 +0100

    [update] : support for arguments:feature/augmentation

--- 001-Augmentation.py
+++ 001-Augmentation.py
@@ -6,105 +6,136 @@ import os
 import random
 import sys
 # number of possible augmentations
+augmentations_total = 7
 
 class ImageAugmentor:
-    def __init__(self, image_path):
+    def __init__(self, image_path, export_folder):
+        """
+        Initializes the ImageAugmentor object.
+
+        Args:
+        - image_path (str): The path to the input image.
+        """
         self.image_name = self.get_image_name(image_path)
-        self.directory  = os.path.dirname(image_path)
-        self.image_extenssion = self.get_image_extenssion(image_path)
+        self.directory = os.path.dirname(image_path) 
+        self.export_directory = os.path.dirname(image_path) if export_folder is None else export_folder
+        self.image_extension = self.get_image_extension(image_path) 
         self.image_path = image_path
         self.image = cv2.imread(image_path)
         self.augmented_images = []  # Initialize with the original image
         
     def get_image_name(self, image_path):
-        # Extract the image name from the path (you may need to adjust this based on your file naming conventions)
+        """
+        Extracts the image name from the path.
+
+        Args:
+        - image_path (str): The path to the input image.
+
+        Returns:
+        - str: The extracted image name.
+        """
         return image_path.split('/')[-1].split('.')[0]
     
-    def get_image_extenssion(self, image_path):
-        # Extract the image name from the path (you may need to adjust this based on your file naming conventions)
+    def get_image_extension(self, image_path):
+        """
+        Extracts the image extension from the path.
+
+        Args:
+        - image_path (str): The path to the input image.
+
+        Returns:
+        - str: The extracted image extension.
+        """
         return image_path.split('/')[-1].split('.')[-1]
     
     def flip_vertical(self):
+        """
+        Applies vertical flipping to the image and appends the result to augmented_images.
+        """
         flipped_image = cv2.flip(self.image, 0)
-        self.augmented_images.append(flipped_image)
+        self.augmented_images.append({'name': 'Flip', 'image': flipped_image})
         
     def rotate(self, angle):
-        # Taking image height and width 
-        imgHeight, imgWidth = self.image.shape[0], self.image.shape[1] 
-    
-        # Computing the centre x,y coordinates 
-        # of an image 
-        centreY, centreX = imgHeight//2, imgWidth//2
-    
-        # Computing 2D rotation Matrix to rotate an image 
-        rotationMatrix = cv2.getRotationMatrix2D((centreY, centreX), angle, 1.0) 
-    
-        # Now will take out sin and cos values from rotationMatrix 
-        # Also used numpy absolute function to make positive value 
-        cosofRotationMatrix = np.abs(rotationMatrix[0][0]) 
-        sinofRotationMatrix = np.abs(rotationMatrix[0][1]) 
-    
-        # Now will compute new height & width of 
-        # an image so that we can use it in 
-        # warpAffine function to prevent cropping of image sides 
-        newImageHeight = int((imgHeight * sinofRotationMatrix) +
-                            (imgWidth * cosofRotationMatrix)) 
-        newImageWidth = int((imgHeight * cosofRotationMatrix) +
-                            (imgWidth * sinofRotationMatrix)) 
-    
-        # After computing the new height & width of an image 
-        # we also need to update the values of rotation matrix 
-        rotationMatrix[0][2] += (newImageWidth/2) - centreX 
-        rotationMatrix[1][2] += (newImageHeight/2) - centreY 
-    
-        # Now, we will perform actual image rotation 
-        rotatingimage = cv2.warpAffine( 
-            self.image, rotationMatrix, (newImageWidth, newImageHeight)) 
-        self.augmented_images.append({'name':'Rotation', 'image':rotatingimage})
+        """
+        Rotates the image by the specified angle and appends the result to augmented_images.
+
+        Args:
+        - angle (float): The angle of rotation.
+        """
+        img_height, img_width = self.image.shape[0], self.image.shape[1]
+        centre_y, centre_x = img_height // 2, img_width // 2
+
+        rotation_matrix = cv2.getRotationMatrix2D((centre_y, centre_x), angle, 1.0)
+        cos_of_rotation_matrix = np.abs(rotation_matrix[0][0])
+        sin_of_rotation_matrix = np.abs(rotation_matrix[0][1])
+
+        new_image_height = int((img_height * sin_of_rotation_matrix) + (img_width * cos_of_rotation_matrix))
+        new_image_width = int((img_height * cos_of_rotation_matrix) + (img_width * sin_of_rotation_matrix))
+
+        rotation_matrix[0][2] += (new_image_width/2) - centre_x
+        rotation_matrix[1][2] += (new_image_height/2) - centre_y
+
+        rotating_image = cv2.warpAffine(self.image, rotation_matrix, (new_image_width, new_image_height))
+        self.augmented_images.append({'name': 'Rotation', 'image': rotating_image})
 
     def scale(self, scale_factor):
+        """
+        Scales the image by the specified factor and appends the result to augmented_images.
+
+        Args:
+        - scale_factor (float): The scaling factor.
+        """
         rows, cols, _ = self.image.shape
-        # Calculate the zoom-in region and resize
         zoomed_image = cv2.resize(self.image[rows//4:3*rows//4, cols//4:3*cols//4], (cols, rows))
-        self.augmented_images.append({'name':'Scaling', 'image': zoomed_image})
+        self.augmented_images.append({'name': 'Scaling', 'image': zoomed_image})
 
     def blur(self):
         """
-        
+        Applies Gaussian blur to the image and appends the result to augmented_images.
         """
         blurred_image = cv2.GaussianBlur(self.image, (15, 15), 0)
-        self.augmented_images.append({'name':'Blur', 'image':blurred_image})
+        self.augmented_images.append({'name': 'Blur', 'image': blurred_image})
 
     def adjust_contrast(self, contrast_factor):
+        """
+        Adjusts the contrast of the image and appends the result to augmented_images.
+
+        Args:
+        - contrast_factor (float): The contrast adjustment factor.
+        """
         adjusted_image = cv2.convertScaleAbs(self.image, alpha=contrast_factor, beta=2)
-        self.augmented_images.append({'name':'Contrast', 'image': adjusted_image})
+        self.augmented_images.append({'name': 'Contrast', 'image': adjusted_image})
 
     def brightness(self, brightness_factor):
+        """
+        Adjusts the brightness of the image and appends the result to augmented_images.
+
+        Args:
+        - brightness_factor (float): The brightness adjustment factor.
+        """
         adjusted_image = cv2.convertScaleAbs(self.image, beta=brightness_factor)
-        self.augmented_images.append({'name':'Brightness', 'image': adjusted_image})
+        self.augmented_images.append({'name': 'Brightness', 'image': adjusted_image})
+
     def projection(self, perspective_factor):
+        """
+        Applies a perspective transformation to the image and appends the result to augmented_images.
+
+        Args:
+        - perspective_factor (float): The perspective transformation factor.
+        """
         rows, cols, _ = self.image.shape
-        # Define source and destination points for perspective transformation
         src_points = np.float32([[0, 0], [cols - 1, 0], [0, rows - 1], [cols - 1, rows - 1]])
-        
-        # Adjust destination points for a more pronounced perspective effect
-        # dst_points = np.float32([[0, 0], [cols - 1, 0], [int(0.3 * cols), rows - 1], [int(0.7 * cols), rows - 1]])
         dst_points = np.float32([
-            [int(0.1 * cols), int(0.3 * rows)],         # Top-left corner
-            [int(0.9 * cols), int(0.1 * rows)],         # Top-right corner
-            [int(0.3 * cols), int(0.9 * rows)],         # Bottom-left corner
-            [int(0.9 * cols), int(0.6 * rows)]          # Bottom-right corner
-        ])            # Calculate perspective transformation matrix
+            [int(0.1 * cols), int(0.3 * rows)],
+            [int(0.9 * cols), int(0.1 * rows)],
+            [int(0.3 * cols), int(0.9 * rows)],
+            [int(0.9 * cols), int(0.6 * rows)]
+        ])
         perspective_matrix = cv2.getPerspectiveTransform(src_points, dst_points)
-
-        # Apply perspective transformation
+                # Apply perspective transformation
         perspective_transformed_image = cv2.warpPerspective(self.image, perspective_matrix, (cols, rows), borderMode=cv2.BORDER_CONSTANT, borderValue=(255, 255, 255))
-
-
         self.augmented_images.append({'name':'Pojection', 'image':perspective_transformed_image})
-    def flip_vertical(self):
-        flipped_image = cv2.flip(self.image, 0)
-        self.augmented_images.append({'name':'Flip', 'image': flipped_image})
+    
 
     def show_augmented_images(self):
         num_images = len(self.augmented_images)
@@ -121,8 +152,7 @@ class ImageAugmentor:
     def save_images(self):
         for image in self.augmented_images:
             # imagePath = f"{self.image_name}_{image['name']}.{self.image_extenssion}"
-            imagePath = f"images/{self.image_name}_{image['name']}.{self.image_extenssion}"
-            print(imagePath)
+            imagePath = f"{self.export_directory}/{self.image_name}_{image['name']}.{self.image_extension}"
             cv2.imwrite(imagePath, image['image'])
             
     def some_augmentations(self, num_operations):
@@ -130,41 +160,85 @@ class ImageAugmentor:
             self.rotate,
             self.blur,
             self.adjust_contrast,
+            self.scale,
+            self.brightness,
+            self.projection,
+            self.flip_vertical,
             # Add other augmentation methods as needed
         ]
-
-        # Randomly shuffle the list of operations
+        # Randomize the list of operations
         random.shuffle(available_operations)
 
         # Apply the selected number of operations
-        for operation in available_operations[:max(num_operations - 1, 0)]:
+        for operation in available_operations[:max(num_operations , 0)]:
             # Generate random parameters if needed for each operation
             if operation.__name__ == 'rotate':
                 angle = random.uniform(-45, 45)
                 operation(angle)
             elif operation.__name__ == 'blur':
                 operation()
+            elif operation.__name__ == 'scale':
+                operation(2)
+            elif operation.__name__ == 'brightness':
+                operation(60)
+            elif operation.__name__ == 'projection':
+                operation(0.8)
+            elif operation.__name__ == 'flip_vertical':
+                operation()
             elif operation.__name__ == 'adjust_contrast':
                 factor = random.uniform(0.5, 2.0)
                 operation(factor)
-            # Add similar conditions for other operations
 
-            
-# Example usage
-image_path = 'images/Apple/Apple_Black_rot/image (13).JPG'
-augmentor = ImageAugmentor(image_path)
 
-# Apply augmentations
-augmentor.some_augmentations(3)
-# augmentor.rotate(-25)
-# augmentor.blur()
-# augmentor.adjust_contrast(1.5)
-# augmentor.scale(2)
-# augmentor.brightness(60)
-# augmentor.projection(0.8)
-# augmentor.flip_vertical()
-augmentor.save_images()
-# augmentor.flip_vertical()
+import sys
+import argparse
 
-# Display original and augmented images in a horizontal plot
-augmentor.show_augmented_images()
+def main():
+    parser = argparse.ArgumentParser(description="Image Augmentation Script")
+    parser.add_argument("input_path", help="Path to the image or directory")
+    parser.add_argument("-size", type=int, default=augmentations_total, help="Number of augmentations to generate (default: 7 and max is number of images multiplied by 7)")
+    parser.add_argument("-export_location", type=str, default=None, help="images export location (default: image or directory path)")
+
+    args = parser.parse_args()
+    input_path = args.input_path
+    if os.path.isfile(input_path):
+        # If the input is a file
+        augment_and_save_single_image(input_path, args.size,  args.export_location)
+
+    elif os.path.isdir(input_path):
+        # If the input is a directory
+        augment_and_save_images_in_directory(input_path, args.size, args.export_location)
+    else:
+        print(f"Error: {input_path} is not a valid file or directory.")
+        sys.exit(1)
+
+def augment_and_save_single_image(image_path, size,  export_path):
+    augmentor = ImageAugmentor(image_path, export_path)
+    for i in range(size):
+        num_augmentations = augmentations_total
+        # Apply some random augmentations
+        augmentor.some_augmentations(num_augmentations)
+        
+        # Save augmented images
+        augmentor.save_images()
+
+def augment_and_save_images_in_directory(directory_path, size, export_path):
+    image_files = [f for f in os.listdir(directory_path) if f.lower().endswith(('.png', '.jpg', '.jpeg'))]
+    files_generated = 0
+    for image_file in image_files:
+        image_path = os.path.join(directory_path, image_file)
+        augmentor = ImageAugmentor(image_path, export_path)
+
+        
+        num_augmentations = augmentations_total if size - files_generated > augmentations_total else size - files_generated
+
+        # Apply some random augmentations
+        augmentor.some_augmentations(num_augmentations)
+        
+        # Save augmented images
+        augmentor.save_images()
+        files_generated += num_augmentations
+
+if __name__ == "__main__":
+    main()
+
