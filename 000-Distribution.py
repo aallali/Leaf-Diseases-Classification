b0b021306aabc3c095d36416270012abd9b555f5
3dida <44850191+aallali@users.noreply.github.com>
2024-01-21 21:50:54 +0100

    [lint] flake8 : @Distribution.py

--- 000-Distribution.py
+++ 000-Distribution.py
@@ -47,7 +47,7 @@ def plot_image_stats(title, stats):
     # Pie chart
     plt.subplot(1, 2, 1)
 
-    pie = plt.pie(counts, labels=categories, autopct="%1.1f%%", colors=colors)
+    plt.pie(counts, labels=categories, autopct="%1.1f%%", colors=colors)
     plt.title("Pie chart")
 
     # Bar chart
@@ -60,7 +60,8 @@ def plot_image_stats(title, stats):
     plt.title("Bar chart")
 
     # Add legends
-    plt.legend(bars, [f"{c.ljust(25)}{counts[i]}" for (i, c) in enumerate(categories)], title="Categories",
+    plt.legend(bars, [f"{c.ljust(25)}{counts[i]}" for (i, c) in
+                      enumerate(categories)], title="Categories",
                bbox_to_anchor=(1.05, 0), loc='lower left')
 
     plt.tight_layout()
@@ -75,16 +76,21 @@ def ft_distribution(target_path, totalVariants, plot_chart=False):
     folders_stats = folders_stats[parent_folder_name]
 
     lowest = 9999999
-    result = {'folder_statistics': {}, 'total_augmentation_to_balance': 0, 'root_path': target_path, 'image_paths': {}}
+    result = {'folder_statistics': {}, 'total_augmentation_to_balance': 0,
+              'root_path': target_path, 'image_paths': {}}
 
     for subFolder in folders_stats:
         total_imgs = folders_stats[subFolder]
         if total_imgs < lowest:
             lowest = total_imgs
-        images_related_to_subfolder = [img for img in all_images if subFolder in img]
+        images_related_to_subfolder = [
+            img for img in all_images if subFolder in img
+        ]
         head, _ = os.path.split(images_related_to_subfolder[0])
         result['image_paths'][subFolder] = {}
-        result['image_paths'][subFolder]['images'] = [os.path.split(img)[1] for img in images_related_to_subfolder]
+        result['image_paths'][subFolder]['images'] = [
+            os.path.split(img)[1] for img in images_related_to_subfolder
+        ]
         result['image_paths'][subFolder]['path_to_folder'] = head
 
     totalToAugment = totalVariants * lowest
@@ -99,14 +105,20 @@ def ft_distribution(target_path, totalVariants, plot_chart=False):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Process images in a specified folder.')
-    parser.add_argument('folder_path', help='Path to the folder containing images.')
+    parser = argparse.ArgumentParser(
+        description='Process images in a specified folder.'
+    )
+    parser.add_argument(
+        'folder_path', help='Path to the folder containing images.'
+    )
 
     args = parser.parse_args()
 
     folder_path = args.folder_path
 
-    distribution = ft_distribution(folder_path, totalVariants=7, plot_chart=True)
+    distribution = ft_distribution(
+        folder_path, totalVariants=7, plot_chart=True
+    )
 
     del (distribution['image_paths'])
     ft_print_pretty(distribution)
