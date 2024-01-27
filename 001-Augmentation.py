1e5374faa2def3946d1cb08365e94584b25b4a05
3dida <44850191+aallali@users.noreply.github.com>
2024-01-27 20:34:20 +0100

    [fix] : export location + data copy  to dest : @ Augmentation.py

--- 001-Augmentation.py
+++ 001-Augmentation.py
@@ -1,8 +1,10 @@
-# write augmentation logic here
+#!/usr/bin/env python3
+
 import importlib
 import argparse
 import os
-from libft import ImageAugmentor
+import shutil
+from libft import ImageAugmentor, ft_clone_folder
 
 Distribution = importlib.import_module("000-Distribution")
 
@@ -11,7 +13,7 @@ AUGMENTATIONS_TOTAL = 6
 
 
 def augment_and_save_single_image(image_path, size,  export_path, plot):
-    augmentor = ImageAugmentor(image_path, "./augmented_directory")
+    augmentor = ImageAugmentor(image_path, export_path)
     # Apply some random augmentations
     augmentor.some_augmentations(size)
     # Save augmented images
@@ -40,13 +42,20 @@ def augment_and_save_images_in_directory(directory_path, size, export_path):
 
 
 def ft_augmentation(input_path, size, export_location):
+    if os.path.exists(export_location):
+        shutil.rmtree(export_location)
+
     if os.path.isfile(input_path):
         # If the input is a file
         augment_and_save_single_image(input_path, size,  export_location,
                                       True)
     elif os.path.isdir(input_path):
+
         # If the input is a directory
-        data = Distribution.ft_distribution(input_path.replace('\\', '/'), 7)
+        data = Distribution.ft_distribution(
+            input_path.replace('\\', '/'),
+            AUGMENTATIONS_TOTAL
+        )
         total_augmentations = data['total_augmentation_to_balance']
 
         for leaf in data['folder_statistics']:
@@ -57,15 +66,25 @@ def ft_augmentation(input_path, size, export_location):
             path_to_folder = data['image_paths'][folderName]['path_to_folder']
             imagePath = ''
             files_generated = 0
+
+            ft_clone_folder(
+                path_to_folder,
+                export_location + path_to_folder.split("/")[-1]
+            )
+
             for image in data['image_paths'][folderName]['images']:
                 imagePath = f"{path_to_folder}/{image}"
-                num_augmentations = AUGMENTATIONS_TOTAL if\
-                    size - files_generated >= AUGMENTATIONS_TOTAL else\
-                    max(size - files_generated, 0)
-                files_generated += num_augmentations
+                num_augmentations = 0
+                if size - files_generated >= AUGMENTATIONS_TOTAL:
+                    num_augmentations = AUGMENTATIONS_TOTAL
+                else:
+                    num_augmentations = max(size - files_generated, 0)
+
                 if (num_augmentations):
                     augment_and_save_single_image(imagePath, num_augmentations,
                                                   export_location, False)
+                    files_generated += num_augmentations
+
         return True
     else:
         print(f"Error: {input_path} is not a valid file or directory.")
@@ -78,14 +97,23 @@ def main():
 
     parser.add_argument("-size", type=int, default=AUGMENTATIONS_TOTAL,
                         help="Number of augmentations to generate\
-                        (default: 7 and max is number of images multiplied\
-                            by 7)")
-    parser.add_argument("-export_location", type=str, default=None,
-                        help="images export location (default: given image or\
-                            directory path)")
+                        (default: 6 and max is number of images multiplied\
+                            by 6)")
+    parser.add_argument(
+        "-l",
+        "--export_location",
+        type=str,
+        default="./augmented_directory/",
+        help="images export location (default: given image or\
+                            directory path)"
+    )
     args = parser.parse_args()
-    input_path, size, export_location = [args.input_path, args.size,
-                                         args.export_location]
+
+    input_path, size, export_location = [
+        args.input_path,
+        args.size,
+        (args.export_location + "/").replace("//", "/")
+    ]
 
     ft_augmentation(input_path, size, export_location)
 
