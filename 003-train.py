d9bfab6c9e08f5db742fcb65bb122acfa1f04a76
MoldyLunchBox <duu116@gmail.com>
2024-02-01 20:00:27 +0100

    [update]:classifier seems to work: feature/training

--- 003-train.py
+++ 003-train.py
@@ -10,6 +10,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from tensorflow.keras.models import load_model
 
+
 class Trainer:
     def __init__(self, data_set_train, data_set_test):
         self.data_set = data_set_train
@@ -18,7 +19,8 @@ class Trainer:
         self.test = data_set_test
         self.model = Sequential()
         self.history = 0
-    
+        self.classes = []
+
     def group_data(self, training, validation):
         train_size = int(len(self.data_set)*.7)
         val_size = int(len(self.data_set)*.3)
@@ -26,26 +28,47 @@ class Trainer:
         self.train = self.data_set.take(train_size)
         self.val = self.data_set.skip(train_size).take(val_size)
         
+    def save_classes(self, path):
+        # Directory containing the dataset
+        dataset_directory = path
+
+        # Get a list of subdirectories (each subdirectory represents a class)
+        class_directories = [d for d in os.listdir(dataset_directory) if os.path.isdir(os.path.join(dataset_directory, d))]
+
+        # Save the class names to the 'classes' attribute
+        self.classes = class_directories
+
+        print("Classes saved:", self.classes)
         
     def build_neural_network_layers(self):
-        self.model.add(Conv2D(16, (3,3), 1, activation='relu', input_shape=(256,256,3)))
-        self.model.add(MaxPooling2D())
-        self.model.add(Conv2D(32, (3,3), 1, activation='relu'))
-        self.model.add(MaxPooling2D())
-        self.model.add(Conv2D(16, (3,3), 1, activation='relu'))
-        self.model.add(MaxPooling2D())
+  
+        self.model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 3)))
+        self.model.add(MaxPooling2D((2, 2)))
+        
+        self.model.add(Conv2D(64, (3, 3), activation='relu'))
+        self.model.add(MaxPooling2D((2, 2)))
+        
+        self.model.add(Conv2D(128, (3, 3), activation='relu'))
+        self.model.add(MaxPooling2D((2, 2)))
+        
+        # Flatten layer
         self.model.add(Flatten())
-        self.model.add(Dense(256, activation='relu'))
-        self.model.add(Dense(8, activation='softmax'))  # 8 classes, softmax activation
-        # self.model.add(Dense(1, activation='sigmoid'))
-        # self.model.compile('adam', loss=tf.losses.BinaryCrossentropy(), metrics=['accuracy'])
-        self.model.compile('adam', loss='categorical_crossentropy', metrics=['accuracy'])
+        
+        # Dense layers
+        self.model.add(Dense(128, activation='relu'))
+        self.model.add(Dense(64, activation='relu'))
+        self.model.add(Dense(8, activation='softmax'))  # Output layer with 8 classes, softmax activation
+        
+        # Compile the model
+        self.model.compile(optimizer='adam',
+                    loss='categorical_crossentropy',  # Categorical cross-entropy loss for multi-class classification
+                    metrics=['accuracy'])
+
 
     def start(self, epoch):
         self.history = self.model.fit(self.train, epochs=epoch, validation_data=self.val)
     def save(self, path):
-        self.modelsmodel.save(os.path.join('models','imageclassifier.h5'))
-        new_model = load_model('imageclassifier.h5')
+        self.model.save(os.path.join('models','imageclassifier.h5'))
         
     def plot_history(self):
         # plot loss
@@ -77,26 +100,29 @@ class Trainer:
 
 
 def main():
-    print("This is the main function")
-    x=0
     training_data = tf.keras.utils.image_dataset_from_directory('augmented_datasets_train_transformed')
     testing_data = tf.keras.utils.image_dataset_from_directory('augmented_datasets_validation')
     data_set = training_data.as_numpy_iterator()
     data_set_testing = testing_data.as_numpy_iterator()
-    training_data = training_data.map(lambda x,y: (x/255, y))
-    testing_data = testing_data.map(lambda x,y: (x/255, y))
-    training_data.as_numpy_iterator().next()
-    testing_data.as_numpy_iterator().next()
+    testing_data.as_numpy_iterator()
+    d = training_data.as_numpy_iterator()
+    batch = d.next()
+    fig, ax = plt.subplots(ncols=4, figsize=(20,20))
 
+    # for idx, img in enumerate(batch[0][:4]):
+    #     ax[idx].imshow(img.astype(int))
+    #     ax[idx].title.set_text(batch[1][idx])
+
+    training_data = training_data.map(lambda x, y: (x / 255, tf.one_hot(y, depth=8)))  # One-hot encoding labels
+    testing_data = testing_data.map(lambda x, y: (x / 255, tf.one_hot(y, depth=8)))      # One-hot encoding labels
     trainer = Trainer(training_data, testing_data)
+    trainer.save_classes('augmented_datasets_train_transformed')
     trainer.group_data(7, 2)
-    print(trainer.data_set)
-    exit()
     trainer.build_neural_network_layers()
-    trainer.start(2)
+    trainer.start(5)
     trainer.plot_history()
-    trainer.testing()
-    trainer.save()
+    trainer.save("path")
+
 if __name__ == "__main__":
     # Call the main function if the script is run directly
     main()
--- 004-predict.py
+++ 004-predict.py
@@ -1,1 +1,32 @@
 # write prediction logic here
+from tensorflow.keras.models import load_model
+import os
+import numpy as np
+import cv2
+
+
+def main():
+    new_model = load_model('models/imageclassifier.h5')
+
+    # Directory containing the dataset
+    dataset_directory = 'augmented_datasets_train_transformed'
+
+    # testign image
+    img = cv2.imread('augmented_datasets_validation/Apple_rust/image (2)_Brightness.JPG')
+
+    # Get a list of subdirectories (each subdirectory represents a class)
+    class_directories = [d for d in os.listdir(dataset_directory) if os.path.isdir(os.path.join(dataset_directory, d))]
+    class_directories.reverse()  # Reverse the list of directories
+
+    # Save the class names to the 'classes' attribute
+    classes = sorted([d for d in os.listdir(dataset_directory) if os.path.isdir(os.path.join(dataset_directory, d))])
+
+    yhat = new_model.predict(np.expand_dims(img/255, 0))
+    predicted_index = np.argmax(yhat)
+    predicted_class = classes[predicted_index]
+
+    print(predicted_class)
+
+if __name__ == "__main__":
+    # Call the main function if the script is run directly
+    main()
