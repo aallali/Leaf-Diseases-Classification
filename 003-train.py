2058ef8cd7092ad0d5498c183f3422222d7a3cf3
MoldyLunchBox <duu116@gmail.com>
2024-02-03 16:39:02 +0100

    [update]: adding parameters: feature/training

--- 003-train.py
+++ 003-train.py
@@ -9,15 +9,19 @@ from tensorflow.keras.metrics import Precision, Recall, BinaryAccuracy
 import numpy as np
 from matplotlib import pyplot as plt
 from tensorflow.keras.models import load_model
+import argparse
 
 
 class Trainer:
-    def __init__(self, data_set_train, data_set_test):
+    def __init__(self, data_set_train, data_set_test, model):
         self.data_set = data_set_train
         self.train = 0
         self.val = 0
         self.test = data_set_test
-        self.model = Sequential()
+        if (model):
+            self.model = tf.keras.models.load_model(model)
+        else:    
+            self.model = Sequential()
         self.history = 0
         self.classes = []
 
@@ -72,6 +76,24 @@ class Trainer:
             self.model.add(Dropout(0.5))  # Dropout layer to prevent overfitting
             self.model.add(Dense(8, activation='softmax'))  # Output layer with 8 classes and softmax activation
 
+        if model_choice == 3:
+            self.model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(256, 256, 3)))
+            self.model.add(MaxPooling2D((2, 2)))
+            
+            self.model.add(Conv2D(64, (3, 3), activation='relu'))
+            self.model.add(MaxPooling2D((2, 2)))
+            
+            self.model.add(Conv2D(128, (3, 3), activation='relu'))
+            self.model.add(MaxPooling2D((2, 2)))
+            
+            # Flatten layer
+            self.model.add(Flatten())
+            
+            # Dense layers
+            self.model.add(Dense(128, activation='relu'))
+            self.model.add(Dense(64, activation='relu'))
+            self.model.add(Dense(8, activation='softmax'))  # Output layer with 8 classes, softmax activation
+  
             # Compile the model with adjusted learning rate
         optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
         self.model.compile(optimizer='adam',
@@ -85,7 +107,9 @@ class Trainer:
     def start(self, epoch):
         self.history = self.model.fit(self.train, epochs=epoch, validation_data=self.val)
     def save(self, path):
-        self.model.save(os.path.join('models','imageclassifier4.h5'))
+        if not os.path.exists(path):
+            os.makedirs(path)
+        self.model.save(os.path.join(path,'imageclassifier.h5'))
         
     def plot_history(self):
         # plot loss
@@ -115,22 +139,38 @@ class Trainer:
             acc.update_state(y, yhat)
         print(pre.result(), re.result(), acc.result())
 
+def main(args):
+    # Load datasets
+    training_data = tf.keras.utils.image_dataset_from_directory(args.training_set)
+    testing_data = tf.keras.utils.image_dataset_from_directory(args.validation_set)
 
-def main():
-    training_data = tf.keras.utils.image_dataset_from_directory('augmented_datasets_train_transformed')
-    testing_data = tf.keras.utils.image_dataset_from_directory('augmented_datasets_validation')
-    testing_data.as_numpy_iterator()
+    # Preprocess datasets
+    training_data = training_data.map(lambda x, y: (x / 255, tf.one_hot(y, depth=8)))
+    testing_data = testing_data.map(lambda x, y: (x / 255, tf.one_hot(y, depth=8)))
 
-    training_data = training_data.map(lambda x, y: (x / 255, tf.one_hot(y, depth=8)))  # One-hot encoding labels
-    testing_data = testing_data.map(lambda x, y: (x / 255, tf.one_hot(y, depth=8)))      # One-hot encoding labels
-    trainer = Trainer(training_data, testing_data)
-    trainer.save_classes('augmented_datasets_train_transformed')
+    # Create and configure trainer object
+    trainer = Trainer(training_data, testing_data, args.model)
+    trainer.save_classes(args.training_set)
     trainer.group_data(7, 2)
-    trainer.build_neural_network_layers(2)
-    trainer.start(5)
+    if (not args.model):
+        trainer.build_neural_network_layers(3)
+    
+    # Start training
+    trainer.start(args.epochs)
     trainer.plot_history()
-    trainer.save("path")
+    
+    # Save trained model
+    trainer.save(args.model_save_location)
+
 
 if __name__ == "__main__":
-    # Call the main function if the script is run directly
-    main()
+    # Set up command-line argument parser
+    parser = argparse.ArgumentParser(description="Train a neural network model")
+    parser.add_argument("--model", type=str, default="", help="Path to the saved model file")
+    parser.add_argument("--epochs", type=int, default=1, help="Number of epochs")
+    parser.add_argument("--training_set", type=str, help="Path to the training dataset")
+    parser.add_argument("--testing_set", type=str, help="Path to the testing dataset")
+    parser.add_argument("--save_model_path", type=str, default="models", help="Path to save the trained model")
+
+    args = parser.parse_args()
+    main(args)
